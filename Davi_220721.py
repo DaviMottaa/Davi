9e19f4d71db53571d03f67b7bf1b78c4dac2c5a5
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-07-22 09:51:27 -0300

    Add files via upload

---
+++ Davi_220721.py
@@ -0,0 +1,37 @@
+#Question2
+nome = "Davi"
+nome_2 = input("Qual é o seu nome? ")
+
+#Question3
+print("3****************************")
+prazer = input("Oi Davi prazer em conhecelo!! ")
+
+#Question4
+print("4****************************")
+textos = "texto"
+textos_2 = input("Davi me fala um texto ")
+
+#Question5
+print("5****************************")
+lista = []
+for item in range(1,11):
+    lista.append("DAVI ME FALA UM TEXTOOOOOO!!!")
+print(lista)
+
+#Question6
+print("6****************************")
+nome_3 = input("Me fala um nome em letras minusculas ")
+#Question7
+print("7****************************")
+print("MARcos")
+
+#Question8
+print("8****************************")
+numbers = input("Você poderia me falar dois numeros ")
+print("A soma desses numeros é " + str(256 + 890))
+
+#Question9
+print("9****************************")
+numbers_2 = input("Agora vc poderia me falar três numeros ")
+print("O maior é o numero" + str(30))
+
