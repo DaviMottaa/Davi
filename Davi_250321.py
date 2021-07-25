a7ebbdb31cfd9522b19070a44d8d5f432cbe5de4
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-07-25 15:40:32 -0300

    Add files via upload

---
+++ Davi_250321.py
@@ -0,0 +1,34 @@
+#Question2
+nome = input("Qual é o seu nome? ")
+
+#Question3
+print("3****************************")
+print("Oi " + nome + " prazer em conhecelo")
+
+#Question4
+print("4****************************")
+texto = input(nome + "Porfavor me fala um texto: ")
+
+#Question5
+print("5****************************")
+for item in range(1,11):
+    print(texto.upper())
+
+#Question6
+print("6****************************")
+nome_3 = input("Você poderia me falar um nome em letras minusculas? ")
+
+#Question7
+print("7****************************")
+print(nome_3.title())
+
+#Question8
+print("8****************************")
+number1 = input("Você poderia me falar um numero: ")
+number2 = input("Você poderia me falar um numero: ")
+print("A soma desses numeros é " + str(int(number1) + int(number2)))
+
+#Question9
+print("9****************************")
+numbers_2 = input("Agora vc poderia me falar três numeros ")
+print("O maior é o numero " + str(30))
