ca2c38ac4242c833d674ffda225f2b055fd222c8
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-07-25 15:28:16 -0300

    Add files via upload

---
+++ Davi_250721.py
@@ -0,0 +1,38 @@
+#Question2
+texto = input("Me fala um texto: ")
+
+#Question3
+print("3*************************")
+print(texto.upper())
+
+#Question4
+print("4*************************")
+strings = input("Me fala 2 strings: ")
+
+#Question5
+print("5*************************")
+
+#Question6
+print("6*************************")
+numero = input("Me fala um numero ")
+
+#Question7
+print("7*************************")
+oito = int(8)
+print(oito)
+#Question8
+print("8*************************")
+print(int(oito + 1))
+
+#Question9
+print("9*************************")
+print(int(oito + 1))
+
+#Question10
+print("10*************************")
+numeros_3 = input("Me fala 2 numeros em linha separadas ")
+lista =[numeros_3]
+print(lista)
+numero1 = input("Adicione um numero ")
+numero2 = input("Adicione um numero ")
+print(numero1 + numero2)
