13635dba11e246448500444f470259a906ed65ef
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-08-03 20:49:52 -0300

    Add files via upload

---
+++ Lição_de_casa.py
@@ -0,0 +1,86 @@
+#Question2
+nome = input("Qual é seu nome em letras minúsculas? ")
+
+#Question3
+sobrenome = input("Qual é seu sobrenome em letras minúsculas? ")
+
+#Question4
+print(nome.title() + " " + sobrenome.title())
+
+#Question5
+lista = []
+for value in range(1,4):
+    nome_2 = input("Me fala tres nomes ")
+    lista.append({"nome":nome_2})
+
+#Question6
+print(lista)
+
+#Question7
+for item in lista:
+    print(nome.title() + " " + sobrenome.title())
+
+#Question8
+numeros = input("Me fala dois numeros ")
+
+#Question9
+
+
+#Question10
+nome = 5
+while nome == 5:
+    if nome > 5:
+        continue
+        print("Davi")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def comprimentando_o_usuario(nome,comprimentar):
+#    """Mostre um comprimento simples"""
+#    print(comprimentar +", "+nome.title()+"!!!")
+#primeiro_nome = "davi"
+#comprimentando_o_usuario(primeiro_nome,"Hello")
+#comprimentando_o_usuario("fernanda","Hello")
+#comprimentando_o_usuario("sander","Hello")
