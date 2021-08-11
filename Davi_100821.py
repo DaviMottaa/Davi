0409f48792162099a2f2d991384cb58b26fadb45
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-08-10 21:47:40 -0300

    Add files via upload

---
+++ Davi_100821.py
@@ -0,0 +1,22 @@
+#Question2
+def comprimentando_usuario(nome,tudo_bem="how where you?",comprimento="Hi"):
+    """comprimentando po usuario"""
+    print(comprimento + " " + nome +" " + tudo_bem)
+lista = ["oi","davi","tudo bem"]
+print(lista)
+
+#Question3
+print("3****************************")
+comprimentando_usuario("Davi","how where you?","Hello")
+
+#Question4
+print("4****************************")
+comprimentando_usuario(nome = "Davi",tudo_bem = "how where you?",comprimento = "Hello")
+
+#Question5
+print("5****************************")
+comprimentando_usuario(comprimento = "Hello",tudo_bem = "how where you?",nome = "Davi")
+
+#Question6
+comprimentando_usuario("Davi")
+
