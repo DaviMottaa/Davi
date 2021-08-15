3e39ed6b51a70d4cb9f12cb7b3d84655150829ab
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-08-15 19:13:45 -0300

    Add files via upload

---
+++ Davi_150821.py
@@ -0,0 +1,41 @@
+#Question2
+def comprimentando(comprimento="Hello",nome="Davi",tudo_bem="como você esta?"):
+    """Comprimentando o usuario"""
+    lista = [comprimento,nome,tudo_bem]
+    print(lista)
+
+#Question3
+print("Question3****************************")
+comprimentando("Hello", "Davi","como você esta")
+
+#Question4
+print("Question4****************************")
+comprimentando(comprimento="Hello",nome="Davi",tudo_bem="como você esta")
+
+#Question5
+print("Question5****************************")
+comprimentando(nome="Davi",comprimento="Hello",tudo_bem="como você esta")
+
+#Question6
+print("Question6****************************")
+comprimentando("Hello","Davi")
+
+#Question7
+print("Question7****************************")
+def S(a,b,c):
+    print(a)
+    print(b)
+    print(c)
+    return a,b,c
+Salada=S(5,6,8)
+print(Salada)
+
+#Question8
+print("Question8****************************")
+S(10,26,34)
+Sábado = 70
+
+
+#Question9
+print("Question9****************************")
+print(Sábado)
