622bfde7134e9a530ca6ade8208a0afd6cf0c80f
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-08-19 21:22:33 -0300

    Add files via upload

---
+++ Davi_190821.py
@@ -0,0 +1,27 @@
+#Question2
+def ab(letras):
+    """Retornar um lista ao dicionario"""
+    letra={"letra":letras[0]}
+    return letra
+muitas_letras = ab(["D"])
+print(muitas_letras)
+
+#Question3
+print("3*******************************")
+for item in range(1,11):
+    print(muitas_letras)
+
+#Question4
+print("4*******************************")
+def dia_semana(Sábado):
+    """Dia da semana"""
+    lista = ["Sexta-feira"]
+    return lista
+varios_dias = dia_semana("Sexta-feira")
+print(varios_dias)
+
+#Question5
+print("5*******************************")
+teste = {}
+for item in range(1,6):
+    print(varios_dias)
