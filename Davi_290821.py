a75678570313ddb0082bb4c02aba36ed9c55905d
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-08-31 20:23:47 -0300

    Add files via upload

---
+++ Davi_290821.py
@@ -0,0 +1,23 @@
+#Question2
+class D():
+    def __init__(self ):
+        self.h = 8
+        self.i = 9
+
+#Question3    
+    def Lição():
+        self.h = h
+
+#Question4
+letra = D()
+print(letra.i)
+
+letra = D()
+print(letra.h)
+
+#Question5
+letra.i = (89)
+print(letra.i)
+
+letra.h = (88)
+print(letra.h)
