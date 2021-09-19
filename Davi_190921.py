f8a8e86fcc82ee1ef365dc174effb2dd8f4235a8
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-09-19 19:17:17 -0300

    Add files via upload

---
+++ Davi_190921.py
@@ -0,0 +1,34 @@
+#Question2
+class A():
+    def __init__(self):
+        print("Davi")
+
+#Question3
+B = A()
+
+#Question4
+B.C = 11
+
+#Question5
+print(B.C)
+
+#Question6
+print("6**************************")
+class X():
+    def __init__(self):
+        self.z = 12
+zx = X()
+print(zx.z)
+
+#Question7
+print("7**************************")
+zx = X()
+
+
+#Question8
+print("8**************************")
+print(zx.z)
+
+#Question9
+print("9**************************")
+print(12)
